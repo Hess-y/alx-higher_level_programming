e209b46125d41adac36684c8725ceabda7e23f1c
Hess-y <hessy683@gmail.com>
2023-01-05 13:50:19 -0800

    task update

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    print(''.join(chr(i) for i in range(ord('a'), ord('z')+1) if i not in (ord('q'), ord('e'))))
+    print(''.join(chr(i) for i in range(97, 123) if chr(i) not in 'qe'))
