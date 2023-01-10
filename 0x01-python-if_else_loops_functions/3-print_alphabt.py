98694b8a336fa22739ac348964e505c2774c26b9
Hess-y <hessy683@gmail.com>
2023-01-10 04:25:03 -0800

    task update

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    print(''.format(chr(i)), if chr(i) not in 'qe')
+    if i == ord(e) or i == ord(q):
+        continue
+    print("{:c}".format(i), end='')
