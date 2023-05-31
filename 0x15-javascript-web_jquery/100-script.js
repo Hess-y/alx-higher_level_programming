61e2b21c66ff4e7058555226dd5d3b060a4b50b3
Hess-y <hessy683@gmail.com>
2023-05-31 05:28:57 -0700

    task gt commit -m task

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <script>
+    // Select the <header> element
+    var header = document.querySelector('header');
+
+    // Update the text color to red
+    header.style.color = '#FF0000';
+  </script>
+</head>
+<body>
+  <header>
+    <!-- Your header content here -->
+  </header>
+</body>
+</html>
---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,5 @@
+// Add click event listener to the div with id "red_header"
+  $('#red_header').on('click', function() {
+    // Select the header element and update the text color to red
+    $('header').css('color', '#FF0000');
+  });
